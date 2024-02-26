0f2c17cf78ae482a071987b7633e6b471f9acf6e
Christoph Khouri <christoph.khouri@gmail.com>
2024-02-26 12:25:03 -0500

    adding raw sql and post id filtering

--- sqlite-viewer.php
+++ sqlite-viewer.php
@@ -18,17 +18,22 @@ function sqlite_menu() {
 }
 
 function sqlite_viewer() {
-  $db = new \PDO('sqlite:' . FQDB);
-
-  $selectedTable = $_POST['table'] ?? null;
-  $ignoreTrashed = isset($_POST['ignoreTrashed']) ? 'checked' : 'checked';
-  $onlyPosts = isset($_POST['onlyPosts']) ? 'checked' : 'checked';
-  $ignoreAutoDrafts = isset($_POST['ignoreAutoDrafts']) ? 'checked' : 'checked';
+  $db               = new \PDO('sqlite:' . FQDB);
+  $query            = null;
+  $sql              = $_POST['sql'] ?? null;
+  $sql              = str_replace('\\', '', $sql);
+  $selectedTable    = $_POST['table'] ?? null;
+  $ignoreTrashed    = isset($_POST['ignoreTrashed']) ? 'checked' : FALSE;
+  $onlyPosts        = isset($_POST['onlyPosts']) ? 'checked' : FALSE;
+  $ignoreAutoDrafts = isset($_POST['ignoreAutoDrafts']) ? 'checked' : FALSE;
+  $postId           = $_POST['post_id'] ?? null;
 
   $tables = $db->query("SELECT name FROM sqlite_master WHERE type='table';")->fetchAll(\PDO::FETCH_ASSOC);
 
   echo '<form method="post" id="dbForm">';
-  echo '<select name="table" id="tableSelect">';
+  // allow raw sql input
+  echo 'SQL: <input type="text" name="sql" id="sql" value="' . @$sql . '">';
+  echo ' or Table:<select name="table" id="tableSelect">';
   foreach ($tables as $table) {
     $selected = $table['name'] === $selectedTable ? ' selected' : '';
     echo '<option' . $selected . '>' . $table['name'] . '</option>';
@@ -36,6 +41,7 @@ function sqlite_viewer() {
   echo '</select>';
 
   echo '<div id="checkboxes" style="display: none">';
+  echo '<label>Post ID <input type="text" name="post_id" value="' . $postId . '"></label>';
   echo '<label><input type="checkbox" name="ignoreTrashed"' . $ignoreTrashed . '> Ignore Trashed</label>';
   echo '<label><input type="checkbox" name="onlyPosts"' . $onlyPosts . '> Only Posts</label>';
   echo '<label><input type="checkbox" name="ignoreAutoDrafts"' . $ignoreAutoDrafts . '> Ignore Auto Drafts</label>';
@@ -50,8 +56,21 @@ function sqlite_viewer() {
     checkboxes.style.display = this.value === "wp_posts" ? "block" : "none";
   });
   </script>';
+  if ($selectedTable === 'wp_posts') {
+    echo '<script>document.getElementById("checkboxes").style.display = "block";</script>';
+  }
 
-  if ($selectedTable) {
+  if ($sql) {
+    $query = $sql;
+    // if string contains wp_posts, add $sql to the query
+    if (strpos($sql, 'wp_posts') !== FALSE) {
+      // search for and add $string directly after the "where" clause
+      $hide = " post_status != 'trash' AND post_status != 'auto-draft' AND ";
+      $query = preg_replace('/(where)/i', '$1 ' . $hide, $query);
+    }
+    // remove \ from the query
+    
+  } else if ($selectedTable) {
     $query = "SELECT * FROM " . $selectedTable;
 
     if ($selectedTable === 'wp_posts') {
@@ -65,11 +84,16 @@ function sqlite_viewer() {
       if ($ignoreAutoDrafts) {
         $conditions[] = "post_status != 'auto-draft'";
       }
+      if ($postId) {
+        $conditions[] = "ID = $postId";
+      }
       if ($conditions) {
         $query .= ' WHERE ' . implode(' AND ', $conditions);
       }
     }
-
+  }
+  if ($query) {
+    echo "$query<hr>";
     $result = $db->query($query);
     $rows = $result->fetchAll(\PDO::FETCH_ASSOC);
 
@@ -87,7 +111,7 @@ function sqlite_viewer() {
     foreach ($rows as $row) {
       echo '<tr>';
       foreach ($row as $cell) {
-        echo '<td>' . $cell . '</td>';
+        echo '<td><xmp>' . $cell . '</xmp></td>';
       }
       echo '</tr>';
     }
